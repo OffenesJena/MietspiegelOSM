6773c8171485ce97657190a406dd1686b491c0c1
Achim Friedland <github@ahzf.de>
2016-03-04 20:18:53 +0100

    Updates for new #ODD16 project

---
+++ MietspiegelOSM/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json.Linq;
+
+using org.GraphDefined.OpenDataAPI.OverpassAPI;
+
+
+namespace MietspiegelOSM
+{
+
+    public class Program
+    {
+
+        public static void Main(String[] Arguments)
+        {
+
+            var JenaId      = new OverpassQuery("Jena").AreaId;
+            var ThüringenId = new OverpassQuery("Thüringen").AreaId;
+
+
+            #region Gebäude
+
+            var a = new OverpassQuery(JenaId).
+                        WithAny      ("building").
+                        ToGeoJSONFile("JenaBuildings.geojson").
+                        Result;
+
+            #endregion
+
+        }
+
+    }
+
+}
